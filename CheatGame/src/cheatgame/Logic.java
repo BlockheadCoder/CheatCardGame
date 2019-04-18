2e813d3e3245d92594ff718ee871ac58c11315c7
LolFuckYouWindows <LolFuckYouWindows@DESKTOP-7CH18QA>
2019-04-18 16:49:11 -0400

    Next turn and Check for win funcitonality added

--- CheatGame/src/cheatgame/Logic.java
+++ CheatGame/src/cheatgame/Logic.java
@@ -1,179 +1,210 @@
 package cheatgame;
 
 import cheatgame.card.CheatCard;
+import cheatgame.card.GUICard;
 import java.io.File;
 import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 
 public class Logic {
 
-   private CheatGame ui;
+    private CheatGame ui;
 
-   private GroupOfCards middleStack;
+    private GroupOfCards middleStack;
 
-   private GroupOfCards currentHand;
-   private ArrayList<GroupOfCards> otherHands;
+    private GroupOfCards currentHand;
+    private ArrayList<GroupOfCards> otherHands;
 
-   private int valueOn = 0;
-   
-   private CheatPlayer currentPlayer;
-   private ArrayList<CheatPlayer> players;
+    private int valueOn = 0;
 
-   private Pane rootPane;
+    private CheatPlayer currentPlayer;
+    private CheatPlayer lastPlayer;
+    private ArrayList<CheatPlayer> players;
 
-   private int numberOfPlayers;
+    private Pane rootPane;
 
-   Logic(CheatGame ui, Pane rootPane, ArrayList<CheatPlayer> players) {
-      this.ui = ui;
-      this.rootPane = rootPane;
-      this.numberOfPlayers = players.size();
-      this.players = players; 
-   }
+    private int numberOfPlayers;
 
-   public void start() {
-      createCardGroups();
-      middleStack.drawCardsOnPane();
-   }
+    Logic(CheatGame ui, Pane rootPane, ArrayList<CheatPlayer> players) {
+        this.ui = ui;
+        this.rootPane = rootPane;
+        this.numberOfPlayers = players.size();
+        this.players = players;
+    }
 
-   public void createCardGroups() {
-      //Image names are {1-52}.png and backV.png
+    public void start() {
+        createCardGroups();
+        middleStack.drawCardsOnPane();
+    }
 
-      String imagesPath = "file:./src/cardImages/";
+    public void createCardGroups() {
+        //Image names are {1-52}.png and backV.png
 
-      Image backImage = new Image(imagesPath + "backV.png");
+        String imagesPath = "file:./src/cardImages/";
 
-      middleStack = new GroupOfCards(
-            rootPane,
-            rootPane.widthProperty().get() / 2,
-            rootPane.heightProperty().get() / 2,
-            2,
-            false);
+        Image backImage = new Image(imagesPath + "backV.png");
 
-      for (int i = 1; i <= 13; i++) {
-         CheatCard newCard = new CheatCard(
-               new Image(imagesPath + String.format("%s.png", i)),
-               backImage, Suit.SPADES, Value.values()[i - 1]);
-         middleStack.addCard(newCard);
+        middleStack = new GroupOfCards(
+                rootPane,
+                rootPane.widthProperty().get() / 2,
+                rootPane.heightProperty().get() / 2,
+                2,
+                false,
+                false);
 
-      }
+        for (int i = 1; i <= 13; i++) {
+            CheatCard newCard = new CheatCard(
+                    new Image(imagesPath + String.format("%s.png", i)),
+                    backImage, Suit.SPADES, Value.values()[i - 1]);
+            middleStack.addCard(newCard);
 
-      for (int i = 14; i <= 26; i++) {
-         CheatCard newCard = new CheatCard(
-               new Image(imagesPath + String.format("%s.png", i)),
-               backImage, Suit.HEARTS, Value.values()[i - 14]);
+        }
 
-         middleStack.addCard(newCard);
+        for (int i = 14; i <= 26; i++) {
+            CheatCard newCard = new CheatCard(
+                    new Image(imagesPath + String.format("%s.png", i)),
+                    backImage, Suit.HEARTS, Value.values()[i - 14]);
 
-      }
+            middleStack.addCard(newCard);
 
-      for (int i = 27; i <= 39; i++) {
-         CheatCard newCard = new CheatCard(
-               new Image(imagesPath + String.format("%s.png", i)),
-               backImage, Suit.DIAMONDS, Value.values()[i - 27]);
+        }
 
-         middleStack.addCard(newCard);
+        for (int i = 27; i <= 39; i++) {
+            CheatCard newCard = new CheatCard(
+                    new Image(imagesPath + String.format("%s.png", i)),
+                    backImage, Suit.DIAMONDS, Value.values()[i - 27]);
 
-      }
+            middleStack.addCard(newCard);
 
-      for (int i = 40; i <= 52; i++) {
-         CheatCard newCard = new CheatCard(
-               new Image(imagesPath + String.format("%s.png", i)),
-               backImage, Suit.CLUBS, Value.values()[i - 40]);
-         middleStack.addCard(newCard);
+        }
 
-      }
+        for (int i = 40; i <= 52; i++) {
+            CheatCard newCard = new CheatCard(
+                    new Image(imagesPath + String.format("%s.png", i)),
+                    backImage, Suit.CLUBS, Value.values()[i - 40]);
+            middleStack.addCard(newCard);
 
-      middleStack.flip(false);
+        }
 
-      currentHand = new GroupOfCards(rootPane, rootPane.widthProperty().get() / 2,
-            rootPane.heightProperty().get() - backImage.getHeight(),
-            25, true);
+        middleStack.flip(false);
 
-      otherHands = new ArrayList(numberOfPlayers);
+        currentHand = new GroupOfCards(rootPane, rootPane.widthProperty().get() / 2,
+                rootPane.heightProperty().get() - backImage.getHeight(),
+                25, true, true);
 
-      
-      
-      double distance = rootPane.widthProperty().get()/2-50;
-      double cX = (rootPane.widthProperty().get()-backImage.getWidth())/2;
-      double cY = rootPane.heightProperty().get()/2;
-      double splitBy = 180 / (numberOfPlayers - 1);
-      
-      
-      
-      for(int i = 0; i < numberOfPlayers; i++){
-         
-         double angle = ((splitBy * i) + 180) * Math.PI / 180;
-         double x = distance * Math.cos(angle) + cX;
-         double y = distance * Math.sin(angle) + cY;
-         
-         otherHands.add(
-               new GroupOfCards(
-                     rootPane, 
-                     x,
-                     y,
-                     4,
-                     false
-               )
-         );
-         
-         players.get(i).addHand(otherHands.get(i));
-         
-      }
-      
-      
-      
-      
-      int numPerHand = 52/players.size();
-      
-      for(int i = 0; i < players.size(); i++){
-         middleStack.moveTo(players.get(i).getHand(), numPerHand);
-      }
-      
-      for(int i = 0; i < 52-(numPerHand*players.size()); i++){
-         middleStack.moveTo(players.get(i).getHand(), 1);
-      }
-      
-      currentPlayer = players.get(0);
-      currentPlayer.getHand().moveAllTo(currentHand);
-      currentHand.flip(true);
-      currentHand.refreshScreen();
-      ui.changeWhosTurnItIs(players.get(0).getPlayerID(), Value.values()[valueOn]);
-      valueOn+=1;
-   }
-   
-   public void nextTurn(){
-      int ind = players.indexOf(currentPlayer);
-      currentHand.flip(false);
-      currentHand.moveAllTo(players.get(ind).getHand());
-      
-      if(ind == players.size() - 1){
-         ind = -1;
-      }
-      currentPlayer = players.get(ind+1);
-      
-      currentPlayer.getHand().moveAllTo(currentHand);
-      
-            currentHand.flip(true);
-      currentHand.refreshScreen();
-      
-      
-      
-      ui.changeWhosTurnItIs(currentPlayer.getPlayerID(), Value.values()[valueOn]);
-      
-      if(valueOn == Value.values().length-1){
-         valueOn = 0;
-      }else{
-         valueOn+=1;
-      }
-      
-      
-   }
-   
-   public void cheatCalled(){
-      
-   }
+        otherHands = new ArrayList(numberOfPlayers);
+
+        for (int i = 0; i < numberOfPlayers; i++) {
+
+            double x = (rootPane.widthProperty().get() / numberOfPlayers * i) + 10;
+            double y = 100;
+
+            otherHands.add(
+                    new GroupOfCards(
+                            rootPane,
+                            x,
+                            y,
+                            4,
+                            false,
+                            false
+                    )
+            );
+
+            players.get(i).addHand(otherHands.get(i));
+
+        }
+
+        middleStack.shuffle();
+
+        int numPerHand = 52 / players.size();
+
+        for (int i = 0; i < players.size(); i++) {
+            middleStack.moveTo(players.get(i).getHand(), numPerHand);
+        }
+
+        for (int i = 0; i < 52 - (numPerHand * players.size()); i++) {
+            middleStack.moveTo(players.get(i).getHand(), 1);
+        }
+
+        currentPlayer = players.get(0);
+        currentPlayer.getHand().moveAllTo(currentHand);
+        currentHand.flip(true);
+        currentHand.refreshScreen();
+        ui.changeWhosTurnItIs(currentPlayer.getPlayerID(), Value.values()[valueOn], "", 0, null);
+    }
+
+    public void nextTurn() {
+
+        ArrayList<GUICard> activatedCards = new ArrayList();
+
+        for (GUICard c : currentHand.showCards()) {
+            if (c.isActivated()) {
+                activatedCards.add(c);
+            }
+        }
+
+        if (activatedCards.isEmpty()) {
+            return;
+        }
+        currentPlayer.played(activatedCards);
+        if(checkForWin()){
+            return;
+        }
+
+        currentHand.moveTo(middleStack, activatedCards);
+
+        int ind = players.indexOf(currentPlayer);
+        currentHand.flip(false);
+        currentHand.moveAllTo(players.get(ind).getHand());
+
+        if (ind == players.size() - 1) {
+            ind = -1;
+        }
+        lastPlayer = currentPlayer;
+        currentPlayer = players.get(ind + 1);
+
+        currentPlayer.getHand().moveAllTo(currentHand);
+
+        currentHand.flip(true);
+        currentHand.refreshScreen();
+        
+        int oldValue = valueOn;
+        if (valueOn == Value.values().length - 1) {
+            valueOn = 0;
+        } else {
+            valueOn += 1;
+        }
+        ui.changeWhosTurnItIs(currentPlayer.getPlayerID(), Value.values()[valueOn], lastPlayer.getPlayerID(), activatedCards.size(), Value.values()[oldValue]);
+        middleStack.flip(false);
+        middleStack.drawCardsOnPane();
+    }
+
+    public boolean checkForWin() {
+        if(lastPlayer == null){
+            return false;
+        }
+        if(lastPlayer.getHand().showCards().size() == 0){
+            Label winner = new Label(lastPlayer.getPlayerID()+" has won the game");
+            winner.setOpacity(1);
+            winner.setFont(new Font(50));
+            winner.setLayoutX(0);
+            winner.setLayoutY(-75);
+            winner.setPrefSize(800,800);
+            rootPane.getChildren().add(winner);
+            currentHand.flip(false);
+            return true;
+        }
+        return false;
+        
+    }
+
+    public void cheatCalled() {
+
+    }
 
 }
