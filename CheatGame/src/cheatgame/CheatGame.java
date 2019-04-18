ecabb97b62e166122b58ed28a67e846c09ac3348
ThomasChapman <>
2019-04-18 17:25:44 -0400

    Finished the final features like calling cheat. Some bug fixes.

--- CheatGame/src/cheatgame/CheatGame.java
+++ CheatGame/src/cheatgame/CheatGame.java
@@ -15,112 +15,125 @@ import javafx.stage.Stage;
 
 public class CheatGame extends Application {
 
-   private Label whosTurnIsIt;
-   private Pane rootPane;
+    private Label whosTurnIsIt;
+    private Label lastPlay;
+    private Pane rootPane;
 
-   public static void main(String[] args) {
-      launch(args);
-   }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-   @Override
-   public void start(Stage primaryStage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-      rootPane = new AnchorPane();
+        rootPane = new AnchorPane();
 
-      rootPane.setStyle("-fx-background-color: #6f9779;");
+        rootPane.setStyle("-fx-background-color: #6f9779;");
 
-      primaryStage.setScene(new Scene(rootPane));
+        primaryStage.setScene(new Scene(rootPane));
 
-      primaryStage.setX(3300); //DEV for development
+        primaryStage.setMinWidth(50);
+        primaryStage.setMinHeight(50);
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(800);
+        rootPane.prefWidthProperty().bind(primaryStage.widthProperty());
+        rootPane.prefHeightProperty().bind(primaryStage.heightProperty());
 
-      primaryStage.setMinWidth(50);
-      primaryStage.setMinHeight(50);
-      primaryStage.setWidth(800);
-      primaryStage.setHeight(800);
-      rootPane.prefWidthProperty().bind(primaryStage.widthProperty());
-      rootPane.prefHeightProperty().bind(primaryStage.heightProperty());
+        primaryStage.show();
 
-      primaryStage.show();
+        whosTurnIsIt = new Label();
+        lastPlay = new Label("");
+        
+        
+        
+        whosTurnIsIt.setText("Setting up game, please follow instructions on popups");
+        whosTurnIsIt.setFont(Font.font(25));
 
-      whosTurnIsIt = new Label();
-      whosTurnIsIt.setText("Setting up game, please follow instructions on popups");
-      whosTurnIsIt.setFont(Font.font(25));
+        rootPane.getChildren().add(whosTurnIsIt);
 
-      rootPane.getChildren().add(whosTurnIsIt);
+        whosTurnIsIt.setLayoutX(0);
+        whosTurnIsIt.setLayoutY(
+                (rootPane.heightProperty().get() / 2) + 150);
 
-      whosTurnIsIt.setLayoutX(0);
-      whosTurnIsIt.setLayoutY(
-            (rootPane.heightProperty().get() / 2)+150);
+        TextInputDialog getNumberOfPlayers = new TextInputDialog("4");
 
-      TextInputDialog getNumberOfPlayers = new TextInputDialog("4");
-      getNumberOfPlayers.setX(primaryStage.getX() + primaryStage.getWidth());
+        getNumberOfPlayers.setTitle("How many players are there?");
+        getNumberOfPlayers.setContentText("Enter number of players (2-10)");
 
-      getNumberOfPlayers.setTitle("How many players are there?");
-      getNumberOfPlayers.setContentText("Enter number of players (2-10)");
+        int numberOfPlayers = -1;
 
-      int numberOfPlayers = -1;
-
-      while (numberOfPlayers == -1) {
-         Optional<String> result = getNumberOfPlayers.showAndWait();
-         if (result.isPresent()) {
-            try {
-               numberOfPlayers = Integer.parseInt(result.get());
-               if (numberOfPlayers < 2 || numberOfPlayers > 10) {
-                  throw new NumberFormatException();
-               }
-            } catch (NumberFormatException e) {
-               getNumberOfPlayers.setContentText("That was an incorrect value\nEnter number of players (2-10)");
-               numberOfPlayers = -1;
+        while (numberOfPlayers == -1) {
+            Optional<String> result = getNumberOfPlayers.showAndWait();
+            if (result.isPresent()) {
+                try {
+                    numberOfPlayers = Integer.parseInt(result.get());
+                    if (numberOfPlayers < 2 || numberOfPlayers > 10) {
+                        throw new NumberFormatException();
+                    }
+                } catch (NumberFormatException e) {
+                    getNumberOfPlayers.setContentText("That was an incorrect value\nEnter number of players (2-10)");
+                    numberOfPlayers = -1;
+                }
+            } else {
+                System.out.println("Exiting");
+                System.exit(1);
             }
-         } else {
-            System.out.println("Exiting");
-            System.exit(1);
-         }
-      }
+        }
 
-      ArrayList<CheatPlayer> pl = new ArrayList();
+        ArrayList<CheatPlayer> pl = new ArrayList();
 
-      for (int i = 0; i < numberOfPlayers; i++) {
-         TextInputDialog getPlayerName = new TextInputDialog();
-         getPlayerName.setHeaderText("Enter your name player: " + (i + 1));
-         getPlayerName.setContentText("Enter your name player: " + (i + 1));
+        for (int i = 0; i < numberOfPlayers; i++) {
+            TextInputDialog getPlayerName = new TextInputDialog();
+            getPlayerName.setHeaderText("Enter your name player: " + (i + 1));
+            getPlayerName.setContentText("Enter your name player: " + (i + 1));
 
-         Optional<String> result = getPlayerName.showAndWait();
-         String playerName = "Player" + (i + 1);
-         if (result.isPresent()) {
-            if (result.get().trim().length() > 0) {
-               playerName = result.get();
+            Optional<String> result = getPlayerName.showAndWait();
+            String playerName = "Player" + (i + 1);
+            if (result.isPresent()) {
+                if (result.get().trim().length() > 0) {
+                    playerName = result.get();
+                }
             }
-         }
 
-         pl.add(new CheatPlayer(playerName));
-      }
+            pl.add(new CheatPlayer(playerName));
+        }
 
-      Button nextTurn = new Button("Play");
-      Button cheat = new Button("CHEAT!");
-      
-      nextTurn.setLayoutX(100);
-      nextTurn.setLayoutY(580);
-      nextTurn.fontProperty().set(Font.font(20));
-      
-      cheat.setLayoutX(350);
-      cheat.setLayoutY(500);
-      cheat.fontProperty().set(Font.font(20));
-      
-      
-      
-      rootPane.getChildren().addAll(nextTurn,cheat);
-      Logic l = new Logic(this, rootPane, pl);
-      l.start();
-      
-      nextTurn.setOnAction(ev -> {
-         l.nextTurn();
-      });
-   }
-   
-   public void changeWhosTurnItIs(String name, Value playing){
-      whosTurnIsIt.setText("It is "+name+"'s turn. Playing :"+playing);
-      whosTurnIsIt.setLayoutX((rootPane.widthProperty().doubleValue()/2) - (whosTurnIsIt.getWidth()/2));
-   }
+        Button nextTurn = new Button("Play");
+        Button cheat = new Button("CHEAT!");
+
+        lastPlay.setLayoutX(150);
+        lastPlay.setLayoutY(200);
+        lastPlay.setFont(Font.font(20));
+
+        nextTurn.setLayoutX(0);
+        nextTurn.setLayoutY(550);
+        nextTurn.setFont(Font.font(30));
+
+        cheat.setLayoutX(350);
+        cheat.setLayoutY(475);
+        cheat.setFont(Font.font(20));
+
+        rootPane.getChildren().addAll(nextTurn, cheat, lastPlay);
+        Logic l = new Logic(this, rootPane, pl);
+        l.start();
+
+        nextTurn.setOnAction(ev -> {
+            l.nextTurn();
+        });
+        
+        cheat.setOnAction(ev -> {
+            l.cheatCalled();
+        });
+    }
+
+    public void changeWhosTurnItIs(String name, Value playing,
+            String lastPlayerName, int numPlayed, Value lastPlayed) {
+
+        if (numPlayed != 0) {
+            lastPlay.setText(String.format("%s claims to have played %d %sS",lastPlayerName,numPlayed,lastPlayed));
+        }
+        whosTurnIsIt.setText("It is " + name + "'s turn. Playing :" + playing);
+        whosTurnIsIt.setLayoutX((rootPane.widthProperty().doubleValue() / 2) - (whosTurnIsIt.getWidth() / 2));
+    }
 
 }
